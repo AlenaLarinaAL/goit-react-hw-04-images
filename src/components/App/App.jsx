1eb464e937a3e20ab6c5217674e2fa6ce53d0360
AlenaLarinaAL <alenalarina605@gmail.com>
2023-08-01 12:38:05 +0000

    add

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -21,7 +21,6 @@ export const App = () => {
   const [query, setQuery] = useState('');
   const [images, setImages] = useState([]);
   const [page, setPage] = useState(1);
-  // const [totalPages, setTotalPages] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
   const [showButton, setShowButton] = useState(false);
   const [showModal, setShowModal] = useState(false);
@@ -30,7 +29,6 @@ export const App = () => {
     if (!query) return;
     setIsLoading(true);
     setShowButton(false);
-
     const getImages = async () => {
       try {
         const { hits, totalHits } = await fetchImages(query, page);
@@ -46,7 +44,6 @@ export const App = () => {
         toast.error(error.message);
       } finally {
         setIsLoading(false);
-        setShowButton(true);
       }
     };
 
@@ -59,7 +56,7 @@ export const App = () => {
     setImages([]);
   };
 
-  const renderImages = () => setPage(page + 1);
+  const renderImages = () => setPage(prevPage => prevPage + 1);
 
   const openModal = (src, alt) => {
     setShowModal({ src, alt });
@@ -94,94 +91,3 @@ export const App = () => {
     </>
   );
 };
-
-// const notify = () => {
-//   toast.info(
-//     'There are not any images for your request...Please make another one'
-//   );
-// };
-
-// export class App extends Component {
-//   state = {
-//     query: '',
-//     images: [],
-//     page: 1,
-//     totalPages: 1,
-//     largeImage: null,
-//     isLoading: false,
-//     showButton: false,
-//   };
-
-//   componentDidUpdate(_, prevState) {
-//     const { query: prevQuery, page: prevPage } = prevState;
-//     const { query, page } = this.state;
-
-//     if (page !== prevPage || query !== prevQuery) {
-//       this.getImages();
-//     }
-//   }
-
-//   async getImages() {
-//     const { query, page, images } = this.state;
-
-//     this.setState({ isLoading: true, showButton: false });
-
-//     try {
-//       const { hits, totalHits } = await fetchImages(query, page);
-
-//       if (!hits.length) {
-//         notify();
-//         return;
-//       }
-
-//       this.setState({
-//         images: [...images, ...hits],
-//         showButton: this.state.page < Math.ceil(totalHits / 12),
-//       });
-//     } catch (error) {
-//       toast.error(error.message);
-//     } finally {
-//       this.setState({ isLoading: false });
-//     }
-//   }
-
-//   handleFormSubmit = query => {
-//     this.setState({ query, page: 1, images: [] });
-//   };
-
-//   setLargeImageURL = url => {
-//     this.setState({ largeImage: url });
-//   };
-
-//   renderImages = () => {
-//     this.setState(state => ({ page: state.page + 1 }));
-//   };
-
-//   render() {
-//     const { images, isLoading, largeImage, showButton } = this.state;
-
-//     return (
-//       <>
-//         <ToastContainer autoClose={3000} />
-//         <Section>
-//           <Searchbar onSubmit={this.handleFormSubmit} />
-
-//           {isLoading && <Spinner />}
-
-//           {images.length > 0 && (
-//             <ImageGallery images={images} onClick={this.setLargeImageURL} />
-//           )}
-
-//           {largeImage && (
-//             <Modal
-//               url={largeImage}
-//               onClose={() => this.setLargeImageURL(null)}
-//             />
-//           )}
-
-//           {showButton && <Button onClick={this.renderImages} />}
-//         </Section>
-//       </>
-//     );
-//   }
-// }
